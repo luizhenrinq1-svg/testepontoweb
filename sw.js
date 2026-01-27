f2b2022e625490890cdde92ba93e94e371b873ce
luizhenrinq1-svg <luiz.henrinq1@gmail.com>
2026-01-27 15:41:58 -0300

    Implement service worker with install and activate events

---
+++ sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  // Quando clica na notificação, foca na janela do Ponto ou abre uma nova
+  event.waitUntil(
+    clients.matchAll({type: 'window', includeUncontrolled: true}).then(function(clientList) {
+      for (var i = 0; i < clientList.length; i++) {
+        var client = clientList[i];
+        if (client.url && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow('./');
+      }
+    })
+  );
+});
