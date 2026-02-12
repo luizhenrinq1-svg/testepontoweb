941071859a9d13d6a7a516af8aabe7a8e10d7a29
luizhenrinq1-svg <luiz.henrinq1@gmail.com>
2026-02-12 14:56:21 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -69,3 +69,7 @@ self.addEventListener('notificationclick', function(event) {
 // === PWA ===
 self.addEventListener('install', (event) => self.skipWaiting());
 self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+
+
+
+
